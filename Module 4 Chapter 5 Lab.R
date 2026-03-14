4acec32f11496c5ad83444f694c9a758ac972a69
rezoan111 <rezoan111@gmail.com>
2026-03-14 14:36:28 -0400

    Add files via upload

---
+++ Module 4 Chapter 5 Lab.R
@@ -0,0 +1,101 @@
+# Problem 2 - Airline Performance Measures
+
+# Part a
+# Delta Air Lines has an on-time arrival rate of 86.5%.
+# So the probability that a randomly chosen Delta flight arrives on time is 0.865.
+
+0.865
+
+# Part b
+# There are 3 airlines out of 10 with less than 2 mishandled baggage reports
+# per 1,000 passengers.
+# So the probability is 3/10 = 0.30.
+
+3/10
+
+# Part c
+# There are 5 airlines out of 10 with more than 1 customer complaint
+# per 1,000 passengers.
+# So the probability is 5/10 = 0.50.
+
+5/10
+
+# Part d
+# AirTran Airways has an on-time arrival rate of 87.1%.
+# The probability that an AirTran flight will not arrive on time
+# is the complement: 1 - 0.871 = 0.129.
+
+1 - 0.871
+
+# Problem 4 - NCAA Basketball Championships
+
+# Part a
+# UCLA won 11 out of the 60 total NCAA Basketball Championships listed.
+# So the probability that the selected winner is from UCLA is 11/60.
+
+11/60
+
+
+# Part b
+# The Big Ten teams listed are Indiana and Michigan State.
+# They won 5 + 2 = 7 championships total.
+# So the probability that the selected winner is from the Big Ten Conference is 7/60.
+
+7/60
+
+# Part c
+# The ACC teams listed are North Carolina, Duke, Louisville, and NC State.
+# They won 6 + 5 + 2 + 2 = 15 championships total.
+# So the probability that the selected winner is from the ACC is 15/60.
+
+15/60
+
+
+# Part d
+# The Big Ten has 7 championships and the ACC has 15 championships.
+# So the probability that the selected winner is from either the Big Ten
+# or the ACC is (7 + 15)/60 = 22/60.
+
+22/60
+
+
+# Problem 8 - Student Loans and College Degrees
+
+
+# Part a
+# From the table, the probability that a student with a student loan
+# had received a college degree is the column total for "Yes".
+# So the probability is 0.42.
+
+0.42
+
+# Part b
+# From the table, the probability that a student with a student loan
+# had not received a college degree is the column total for "No".
+# So the probability is 0.58.
+
+0.58
+
+# Part c
+# Given that the student has received a college degree,
+# the probability that the student has a delinquent loan is:
+# 0.16 / 0.42 = 0.3810
+
+0.16 / 0.42
+
+
+# Part d
+# Given that the student has not received a college degree,
+# the probability that the student has a delinquent loan is:
+# 0.34 / 0.58 = 0.5862
+
+0.34 / 0.58
+
+# Part e
+# Dropping out of college without a degree increases the chance
+# of having a delinquent student loan.
+# Students without a degree have a delinquent loan probability of 0.5862,
+# while students with a degree have a delinquent loan probability of 0.3810.
+# So students without a degree are more likely to have a delinquent loan.
+
+0.5862 - 0.3810
